59dc60bbc77f2a9d951952e971ff4787a29ddcc5
paigemaccc <158656875+paigemaccc@users.noreply.github.com>
2024-02-03 02:55:47 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    string firstname = "Paige";
+    string lastname = "Mac";
+    int age = 20;
+    Console.WriteLine ($"Hello World {firstname}{lastname} age: ");
+    Console.WriteLine ("Hello World" +  firstname + " " + lastname + " " + age);
+  }
+}
